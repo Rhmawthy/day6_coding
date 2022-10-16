9fe7842dae877b3ee0b1457be057d827b29f7a97
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-16 18:30:59 +0800

    Add files via upload

---
+++ day6_operator aritmatika.py
@@ -0,0 +1,33 @@
+#operator aritmatika
+#contoh sederhana penggunaan operator aritmatika
+
+a = 20
+b = 10
+#operator pejumlahan (+)
+
+print( a + b )
+
+#operator penguragan (-)
+
+print (a - b )
+
+#operator perkalian (*)
+
+print ( a * b )
+
+#operator pembagian (/)
+
+print ( a / b )
+
+#operator sisah bagi (%)
+
+print ( a % b )
+
+#operator pankat (**)
+print ( a ** b )
+
+#operator pembagian bulat
+print ( a // b)  
+
+
+
